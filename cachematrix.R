358efbbba62ebb6e6284b14df75b5bcb0b36176b
Harikrishnan G <35332275+harikrishnang3399@users.noreply.github.com>
2020-05-28 16:12:16 +0530

    Second commit
    Added description of functions used and what is done.

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,15 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {      #Here is defined the x variable (empty)
   inv <- NULL
   set <- function(y) {
-    x <<- y
+    x <<- y                                      #This is assigned a value from some parental env to x variable
     inv <<- NULL
   }
-  get <- function() x
-  setsolve <- function(solve) inv <<- solve
-  getsolve <- function() inv
+  get <- function() x                            #This function only retrieves the x value
+  setsolve <- function(solve) inv <<- solve      #This function assigns the solve value to the "inv" variable somewhere in parent env
+  getsolve <- function() inv                     #This line retrieves the the inverted matrix
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
@@ -23,12 +23,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getsolve()
-  if(!is.null(inv)) {
+  if(!is.null(inv)) {                           #If inverted matrix already exist (cached), the message and matrix will be retrieved
     message("getting cached data")
     return(inv)
   }
   data <- x$get()
-  inv <- solve(data, ...)
-  x$setsolve(inv)
+  inv <- solve(data, ...)                       #Here the matrix is inverted
+  x$setsolve(inv)                               #Storing inverted matrix
   inv
 }
